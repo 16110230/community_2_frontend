e8976e1c044abc0591aabf4396b342af9c42f463
firdaus <a.firdaus@gmail.com>
2022-07-26 11:11:34 +0700

    oke

--- src/app/page/admin/admin-activity-events-component/admin-activity-events.component.ts
+++ src/app/page/admin/admin-activity-events-component/admin-activity-events.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from "@angular/core";
-import { PojoActivity } from "src/app/pojo/activity/pojoActivity";
+import { PojoActivity } from "src/app/dto/activity/activity-dto";
 
 @Component({
     selector: "app-admin-activity-events",
