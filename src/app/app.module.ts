b5313b38efb5e163e3a0366c87da5489d98e2cdf
Ardy Sitio <ardy.ai29@gmail.com>
2022-07-27 18:14:01 +0700

    add interceptor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { BrowserAnimationsModule } from "@angular/platform-browser/animations"
 
 import { AppComponent } from './app.component';
 import { AppRouting } from './app.routing';
+import { CustomInterceptor } from "./interceptor/custom.interceptor"
 
 @NgModule({
   declarations: [
@@ -18,7 +19,9 @@ import { AppRouting } from './app.routing';
     HttpClientModule,
     BrowserAnimationsModule
   ],
-  providers: [],
+  providers: [
+    {provide : HTTP_INTERCEPTORS, useClass : CustomInterceptor, multi : true}
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
