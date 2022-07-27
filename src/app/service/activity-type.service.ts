fc235b17e28736c2c819fcbbd4985844554d2a94
firdaus <a.firdaus@gmail.com>
2022-07-27 11:57:56 +0700

    add service

---
+++ src/app/service/activity-type.service.ts
@@ -0,0 +1,39 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { BASE_URL } from "../constant/constant";
+import { InsertActyivityTypeReq } from "../dto/activity-type/insert-activity-type-req";
+import { ShowActivityTypeById } from "../dto/activity-type/show-activity-type-by-id";
+import { ShowActivityTypes } from "../dto/activity-type/show-activity-types";
+import { UpdateActivityTypeReq } from "../dto/activity-type/update-activity-type-req";
+import { DeleteResDto } from "../dto/delete-res-dto";
+import { InsertResDto } from "../dto/insert-res-dto";
+import { UpdateResDto } from "../dto/update-res-dto";
+
+@Injectable({
+    providedIn : 'root'
+})
+export class ActivityTypeService{
+
+    constructor(private http : HttpClient) {}
+
+    getAll() : Observable<ShowActivityTypes> {
+        return this.http.get<ShowActivityTypes>(`${BASE_URL}/activity-types`)
+    }
+
+    getById(id : number) : Observable<ShowActivityTypeById> {
+        return this.http.get<ShowActivityTypeById>(`${BASE_URL}/activity-types/${id}`)
+    }
+
+    insert(data : InsertActyivityTypeReq) : Observable<InsertResDto> {
+        return this.http.post<InsertResDto>(`${BASE_URL}/activity-types`, data)
+    }
+
+    update(data : UpdateActivityTypeReq) : Observable<UpdateResDto> {
+        return this.http.put<UpdateResDto>(`${BASE_URL}/activity-types`, data)
+    }
+
+    delete(id : number) : Observable<DeleteResDto> {
+        return this.http.delete<DeleteResDto>(`${BASE_URL}/activity-types/${id}`)
+    }
+}
