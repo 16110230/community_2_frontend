3a47e1e9c4781728d7071a069a3e3064f42be4d9
copi <mtaufikdhrmwn@gmail.com>
2022-08-08 13:12:00 +0700

    fix bug thread insert

--- src/app/dto/polling/insert-polling-main-req.ts
+++ src/app/dto/polling/insert-polling-main-req.ts
@@ -2,6 +2,6 @@ import { InsertPollingDtlReq } from "./insert-polling-dtl-req";
 import { InsertPollingHdrReq } from "./insert-polling-hdr-req";
 
 export interface InsertPollingMainReq{
-    header? : InsertPollingHdrReq
-	details? : InsertPollingDtlReq[]
+    header : InsertPollingHdrReq
+	details : InsertPollingDtlReq[]
 }
