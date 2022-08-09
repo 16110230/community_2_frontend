dc1c746c988910ce66c094d4c4e3afcfb2e53893
Shifda Ichfani <shifda.ichfani@gmail.com>
2022-08-09 17:41:24 +0700

    9-8-22 evening

--- src/app/page/member/articles/article-detail/article-detail.component.ts
+++ src/app/page/member/articles/article-detail/article-detail.component.ts
@@ -1,8 +1,41 @@
-import { Component } from "@angular/core";
+import { Component, OnDestroy, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { Subscription } from "rxjs";
+import { ShowThreadById } from "src/app/dto/thread/show-thread-by-id";
+import { ThreadService } from "src/app/service/thread.service";
 
 @Component({
     selector: 'app-article-detail',
     templateUrl: './article-detail.component.html',
     styleUrls: ['../article.component.css']
 })
-export class ArticleDetailComponent {}
+export class ArticleDetailComponent implements OnInit, OnDestroy {
+
+    constructor(
+        private activatedRoute: ActivatedRoute,
+        private threadService: ThreadService
+    ) { }
+
+    thread: ShowThreadById = {} as ShowThreadById
+    threadSub?: Subscription
+    idParam?: string
+
+    ngOnInit() {
+        this.getData()
+    }
+
+    ngOnDestroy() {
+        this.threadSub?.unsubscribe()
+    }
+
+    getData() {
+        this.activatedRoute.params.subscribe(result => {
+            const resultTemp: any = result
+            this.idParam = resultTemp.id
+
+            this.threadSub = this.threadService.getById(this.idParam!).subscribe(res => {
+                this.thread.data = res.data
+            })
+        })
+    }
+}
