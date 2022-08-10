00e525099d532189ea53702141ba272de5193ee5
firdaus <a.firdaus@gmail.com>
2022-08-10 17:59:17 +0700

    detail repost downloadble with date filter

--- src/app/page/member/member-profile/member-profile.routing.ts
+++ src/app/page/member/member-profile/member-profile.routing.ts
@@ -1,6 +1,7 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
 import { AuthLoginGuard } from "src/app/guard/auth-login.guard";
+import { MemberProfileActivityDetailComponent } from "./member-profile-activity-detail/activity-detail.component";
 import { MemberProfileActivityComponent } from "./member-profile-activity/activity.component";
 import { MemberProfileChangePassword } from "./member-profile-change-password/member-profile-change-password.component";
 import { MemberProfileEditComponent } from "./member-profile-edit/member-profile-edit.component";
@@ -24,6 +25,10 @@ const routes: Routes = [
     {
         path: 'activity',
         component: MemberProfileActivityComponent
+    },
+    {
+        path: 'activity/detail/:id',
+        component: MemberProfileActivityDetailComponent
     },
     {
         path: 'transaction',
